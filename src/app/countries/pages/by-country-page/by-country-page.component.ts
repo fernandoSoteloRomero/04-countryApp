f0e6e88a1f968f0a6b1c77a2e9e9542b88cc90a7
fernando sotelo <al169888@alumnos.uacj.mx>
2024-04-19 13:34:56 -0600

    Por Pais

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-country-page',
   templateUrl: './by-country-page.component.html'
 })
 export class ByCountryPageComponent {
+
+  constructor(private countriesService:CountriesService){}
+
+  public countries: Country[] = [];
+
+
+  searchByCountry(value: string): void{
+    this.countriesService.searchCountry(value)
+    .subscribe(countries => {
+      this.countries = countries;
+    })
+    
+  }
 
 }
